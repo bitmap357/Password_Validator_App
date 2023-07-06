b763c1d8d0ce23df85634dbc3f2e0e2e285e7dfb
bitmap357 <edakoyeram@gmail.com>
2023-07-06 22:11:02 +0000

    function to validate

--- Password app.py
+++ Password app.py
@@ -5,8 +5,10 @@ import bcrypt
 def validate(password):
     hash = b'$2b$12$zHXl32aL20iFa6HYSubn4Ogvrjj6BYl1AX4nb7CnI63ieBaLe67Wm'
     password = bytes(password, encoding='utf-8')
-
-
+    if bcrypt.checkpw(password, hash):
+        print("Log in successful")
+    else:
+        print("Invalid password")
 
 
 root = Tk()
