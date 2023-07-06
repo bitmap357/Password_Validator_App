3ee4a26fd4dfe9e337e6d70044e69f7728b3a5b2
bitmap357 <edakoyeram@gmail.com>
2023-07-06 22:06:03 +0000

    button command

--- Password app.py
+++ Password app.py
@@ -7,7 +7,7 @@ root.geometry('300x300')
 password_entry = Entry(root)
 password_entry.pack()
 
-button = Button(root, text='Validate', command=)
+button = Button(root, text='Validate', command=validate)
 button.pack()
 
 
