9d9b34dd5295f41190a4cf796d54df8e9ecb424b
bitmap357 <edakoyeram@gmail.com>
2023-07-06 22:07:25 +0000

    correction

--- Password app.py
+++ Password app.py
@@ -7,7 +7,7 @@ root.geometry('300x300')
 password_entry = Entry(root)
 password_entry.pack()
 
-button = Button(root, text='Validate', command=validate)
+button = Button(root, text='Validate', command=lambda: validate(password_entry.get()))
 button.pack()
 
 
